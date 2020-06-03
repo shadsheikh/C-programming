70bf6ce670bb2935ae8ccb575f9eba4f6cdb48a0
Coursera Learner <nobody@nowhere.nul>
2020-06-03 17:50:13 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,13 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-struct _retire_info {
+typedef struct Retire_info {
   int months;
   double contribution;
   double rate_of_return;
-};
+}retire_info;
 
-typedef struct _retire_info retire_info;
 
 void print_monthly_info(int months, double balance) {
   printf("Age %3d month %2d you have $%.2lf\n",months / 12, months % 12, balance);
