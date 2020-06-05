eea45f303da3b43053ba5638f4c895233a5e8c8e
Coursera Learner <nobody@nowhere.nul>
2020-06-05 14:36:19 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,15 +1,14 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct Retire_info {
+typedef struct Retire {
   int months;
   double contribution;
   double rate_of_return;
 }retire_info;
 
-
 void print_monthly_info(int months, double balance) {
-  printf("Age %3d month %2d you have $%.2lf\n",months / 12, months % 12, balance);
+  printf("Age %3d month %2d you have $%.2lf \n",months / 12 ,months % 12 ,balance);
 
   return;
 }
@@ -21,24 +20,25 @@ double balance_calc(double balance, retire_info retire_stats) {
   return balance;
 }
 
-void retirement(int starAge, double initial, retire_info working, retire_info retired) {
-  double balance = initial;
-  int total_months = starAge - 1;
 
-  for (int i = 0; i <  working.months; i++) {
+
+void  retirement(int startAge, double initial, retire_info working, retire_info retired) {
+  double balance = initial;
+  int total_months = startAge - 1;
+
+  for(int i = 0; i < working.months; i++) {
     total_months += 1;
-    print_monthly_info(total_months, balance);
+    print_monthly_info(total_months,balance);
     balance = balance_calc(balance, working);
   }
 
-  
-  for (int j = 0; j <  retired.months; j++) {
+   for(int j = 0; j < retired.months; j++) {
     total_months += 1;
-    print_monthly_info(total_months, balance);
+    print_monthly_info(total_months,balance);
     balance = balance_calc(balance, retired);
   }
-
-  return;
+   return ;
+ 
 }
 
 int main() {
@@ -49,12 +49,11 @@ int main() {
 
   retire_info retired;
   retired.months = 384;
-  retired.contribution= -4000;
+  retired.contribution = -4000;
   retired.rate_of_return = 0.01 / 12.0;
 
-  retirement(327, 21345, working, retired);
+  retirement(327 , 21345 , working ,retired);
 
   return 0;
 }
   
- 
