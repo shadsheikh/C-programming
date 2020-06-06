d12451a4d5c0586bd415bba12d771dd7e6385613
Coursera Learner <nobody@nowhere.nul>
2020-06-06 15:54:15 +0000

    Did project c2prj1_cards

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,10 +5,28 @@
 
 
 void assert_card_valid(card_t c) {
-  assert(c.value >= 2 && c.value <= VALUE_ACE);
-  assert(c.suit >= SPADES && c.suit <= CLUBS);
-
+  int v;
+  int vvalidity = 0;
+  for (v=2;v<=VALUE_ACE;v++) {
+    if (c.value ==v) {
+      vvalidity = 1;
+    
+    break;
+  }
+  
 }
+
+  assert(vvalidity == 1);
+int s;
+int svalidity = 0;
+for(s=0;s<NUM_SUITS;s++) {
+    if(c.suit == s) {
+      svalidity = 1;
+      break;
+    }
+  }
+  assert(svalidity == 1);
+ }
 
 const char * ranking_to_string(hand_ranking_t r) {
  
@@ -30,75 +48,95 @@ const char * ranking_to_string(hand_ranking_t r) {
     return "TWO_PAIR";
       case PAIR:
     return "PAIR";
-  default:
+      case NOTHING:
     return "NOTHING";
+      default:
+    return "INVALID";
   }
-
-
-
 }
 
 char value_letter(card_t c) {
-  static const char* letters = "234567890JQKA";
-  if (c.value >= 2&& c.value <= VALUE_ACE) {
-    return letters[c.value -2];
-  }else {
-    return '?';
+  char x;
+  if((c.value >= 2)&&(c.value<=9)) {
+    x='0' +c.value;
   }
-
+  else{
+    switch (c.value) {
+    case 10:         x='0'; break;
+    case VALUE_JACK: x='J'; break;
+    case VALUE_QUEEN:x='Q'; break;
+    case VALUE_KING: x='K'; break;
+    case VALUE_ACE:  x='A'; break;
+    default:         x='X'; break;
+    }
+  }
+  return x;
 }
 
 char suit_letter(card_t c) {
-  static const char* suit_letters = "shdc";
-  return (c.suit >= 0 && c.suit <=CLUBS)?
-    suit_letters[c.suit]: '?' ;
-  
-}
+  char x;
+  switch(c.suit){
+  case SPADES:    x= 's' ;   break;
+  case HEARTS:     x= 'h' ;   break;
+  case DIAMONDS:  x= 'd' ;   break;
+  case CLUBS:     x= 'c' ;   break;
+  default :       x= 'x' ;   break;
+  }
+  return x;
+
+  }
 
 void print_card(card_t c) {
-
-  printf("%c%c" , value_letter(c),suit_letter(c));
+  char value = value_letter(c);
+  char suit  = suit_letter(c);
+  printf("%c%c" , value,suit);
   
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
-  static const char* value_letters = "234567890JQKA";
-  static const char* suit_letters ="shdc";
    card_t temp;
-   int i;
-
-   temp.value = 99;
-   for (i=0;i<13;i++) {
-     if (value_letters[i] == value_let) {
-       temp.value = i + 2  ;
-       break;
-     }
+   if(value_let >= '2' && value_let <= '9') {
+     temp.value = value_let - '0';
    }
 
-   if(temp.value == 99) {
-     printf("Invalid value letter : %c \n",value_let);
-     exit(EXIT_FAILURE);
+   else if (value_let == '0' || value_let == 'J' || value_let == 'Q'||value_let == 'K'||value_let == 'A') {
+     switch (value_let) {
+ case '0': temp.value =10;          ; break;
+ case 'J': temp.value =VALUE_JACK   ; break;
+ case 'Q': temp.value =VALUE_QUEEN  ; break;
+ case 'K': temp.value =VALUE_KING   ; break;
+ case 'A': temp.value =VALUE_ACE    ; break;
+ default : temp.value = 0;
+}
+}
+
+   else {
+     printf("invalid value \n");
+     exit (EXIT_FAILURE);
    }
 
-   temp.suit = NUM_SUITS;
-   for(i=0;i<4;i++) {
-     if (suit_letters[i] == suit_let) {
-       temp.suit = i;
-       break;
-     }
+   switch (suit_let) {
+   case 's' : temp.suit = SPADES ; break;
+   case 'h' : temp.suit = HEARTS ; break;
+   case 'd' : temp.suit = DIAMONDS ; break;
+   case 'c' : temp.suit = CLUBS ; break;
+   default : exit(EXIT_FAILURE);
    }
-   if(temp.suit == NUM_SUITS) {
-     printf("Invalid suit letter : %c\n",suit_let);
-     exit(EXIT_FAILURE);
-   }
-     
    return temp;
 }
 
+
 card_t card_from_num(unsigned c) {
   card_t temp;
-  temp.suit = c / 13;
-  temp.value = c % 13 + 2;
-  
-  return temp;
+  unsigned suitNum = c /13;
+  unsigned valNum = c % 13;
+  switch (suitNum) {
+  case 0: temp.suit = SPADES; break;
+  case 1: temp.suit = HEARTS; break;
+  case 2: temp.suit = DIAMONDS; break;
+  case 3: temp.suit = CLUBS ; break;
+  default : exit(EXIT_FAILURE);
+      }
+  temp.value = valNum + 2;
+    return temp;
 }
