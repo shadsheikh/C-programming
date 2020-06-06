7133953734a1cf12bc5f377474852b908f316dde
Coursera Learner <nobody@nowhere.nul>
2020-06-06 16:28:56 +0000

    Did project c2prj

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,11 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 #include "cards.h"
 
-int main(void) {
-  card_t c = card_from_num(20);
-  assert_card_valid(c);
-  print_card(c);
 
-  card_t c2 = card_from_letters('Q' , 'c');
+int main(void) {
+  card_t c1,c2,c3;
+  c1.value = 9;
+  c1.suit = SPADES;
+  c2.value =1;
+  c2.suit = SPADES;
+  c3.value = VALUE_KING;
+  c3.suit = NUM_SUITS;
+  
+  assert_card_valid(c1);
   assert_card_valid(c2);
-  print_card(c2);
+  assert_card_valid(c3);
+
+  return EXIT_SUCCESS;
 }
