6f17eb6b6bc246f722812910dde75881ef03410c
Coursera Learner <nobody@nowhere.nul>
2020-06-07 16:20:45 +0000

    Did assignment 22

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -3,32 +3,32 @@
 
 unsigned power(unsigned x, unsigned y);
 
-int mian(void) {
+int main(void) {
   unsigned ans ;
 
-  ans = power(0,0);
-  if (ans !=1) return EXIT_FAILURE;
+ans = power(0,0);
+  if (ans != 1) return EXIT_FAILURE;
 
 ans = power(5,0);
-  if (ans !=1) return EXIT_FAILURE;
+  if (ans != 1) return EXIT_FAILURE;
 
 ans = power(0,5);
-  if (ans !=0) return EXIT_FAILURE;
+  if (ans != 0) return EXIT_FAILURE;
 
 ans = power(5,2);
-  if (ans !=25) return EXIT_FAILURE;
+  if (ans != 25) return EXIT_FAILURE;
 
 ans = power(2*3,2);
-  if (ans !=36) return EXIT_FAILURE;
+  if (ans != 36) return EXIT_FAILURE;
 
 ans = power(5,1);
-  if (ans !=5) return EXIT_FAILURE;
+  if (ans != 5) return EXIT_FAILURE;
 
 ans = power(2,10);
-  if (ans !=1024) return EXIT_FAILURE;
+  if (ans != 1024) return EXIT_FAILURE;
 
 ans = power(10,2);
-  if (ans !=100) return EXIT_FAILURE;
+  if (ans != 100) return EXIT_FAILURE;
 
 ans = power(10,5);
   if (ans !=100000) return EXIT_FAILURE;
