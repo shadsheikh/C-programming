712db1c8ad605bf2c88e8f40aa31b786dd98b59d
Coursera Learner <nobody@nowhere.nul>
2020-06-08 05:01:21 +0000

    Did project c3prj1_deck

--- c3prj1_deck/deck.h
+++ c3prj1_deck/deck.h
@@ -1,4 +1,4 @@
-//#ifndef DECK_H
+#ifndef DECK_H
 #define DECK_H
 #include <stdlib.h>
 #include "cards.h"
@@ -13,11 +13,10 @@ int deck_contains(deck_t * d, card_t c) ;
 void shuffle(deck_t * d);
 void assert_full_deck(deck_t * d) ;
 //The below functions will be done in course 4.
-
-//deck_t * make_deck_exclude(deck_t * excluded_cards);
-//void add_card_to(deck_t * deck, card_t c);
-//card_t * add_empty_card(deck_t * deck);
+deck_t * make_deck_exclude(deck_t * excluded_cards);
+void add_card_to(deck_t * deck, card_t c);
+card_t * add_empty_card(deck_t * deck);
 void free_deck(deck_t * deck) ;
-//deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) ;
-//#endif
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) ;
+#endif
 
