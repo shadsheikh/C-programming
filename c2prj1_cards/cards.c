0e33f90d5fd096d447966c5e2f40ffa6711142f4
Coursera Learner <nobody@nowhere.nul>
2020-06-12 15:11:00 +0000

    Did project c2prj1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,28 +5,11 @@
 
 
 void assert_card_valid(card_t c) {
-  int v;
-  int vvalidity = 0;
-  for (v=2;v<=VALUE_ACE;v++) {
-    if (c.value ==v) {
-      vvalidity = 1;
-    
-    break;
-  }
-  
+  int cond1,cond2;
+  cond1=(c.value >=2 )&&(c.value <= VALUE_ACE);
+  cond2=(c.suit == SPADES )||(c.suit == CLUBS )||(c.suit == HEARTS)||(c.suit == DIAMONDS);
+  assert(cond1 && cond2);
 }
-
-  assert(vvalidity == 1);
-int s;
-int svalidity = 0;
-for(s=0;s<NUM_SUITS;s++) {
-    if(c.suit == s) {
-      svalidity = 1;
-      break;
-    }
-  }
-  assert(svalidity == 1);
- }
 
 const char * ranking_to_string(hand_ranking_t r) {
  
@@ -40,93 +23,80 @@ const char * ranking_to_string(hand_ranking_t r) {
   case THREE_OF_A_KIND:   return "THREE_OF_A_KIND" ;break;
   case TWO_PAIR:          return "TWO_PAIR";        break;
   case PAIR:              return "PAIR";            break;
-  case NOTHING:           return "NOTHING";         break;
-  default:                return "INVALID";        
+  default:                return "NOTHING";        
    }
 }
 
 char value_letter(card_t c) {
-  char x;
-  if((c.value >= 2)&&(c.value<=9)) {
-    x='0' +c.value;
-  }
+  char x='?';
+  if((c.value >= 2)&&(c.value<=9)) x = '0' + c.value;
   else{
     switch (c.value) {
-    case 10:         x='0'; break;
-    case VALUE_JACK: x='J'; break;
-    case VALUE_QUEEN:x='Q'; break;
     case VALUE_KING: x='K'; break;
+    case 10:         x='0'; break;
     case VALUE_ACE:  x='A'; break;
-    default:         x='X'; break;
+    case VALUE_QUEEN:x='Q'; break;
+    case VALUE_JACK: x='J'; break;
+    default: break;
     }
   }
   return x;
 }
 
 char suit_letter(card_t c) {
-  char x;
+  char x='?';
   switch(c.suit){
   case SPADES:    x= 's' ;   break;
-  case HEARTS:     x= 'h' ;   break;
+  case HEARTS:    x= 'h' ;   break;
   case DIAMONDS:  x= 'd' ;   break;
   case CLUBS:     x= 'c' ;   break;
-  default :       x= 'x' ;   break;
+  default :       x= 'N' ;   break;
   }
   return x;
 
   }
 
 void print_card(card_t c) {
-  char value = value_letter(c);
-  char suit  = suit_letter(c);
-  printf("%c%c " , value,suit);
+  char suit1  = suit_letter( c);
+  char value1  = value_letter( c);
+  printf("%c%c " ,value1,suit1);
   
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
    card_t temp;
-   if(value_let >= '2' && value_let <= '9') {
-     temp.value = value_let - '0';
-   }
-
-   else if (value_let == '0' || value_let == 'J' || value_let == 'Q'||value_let == 'K'||value_let == 'A') {
-     switch (value_let) {
+ switch (value_let) {
+ case 'K': temp.value =13;          ; break;
  case '0': temp.value =10;          ; break;
- case 'J': temp.value =VALUE_JACK   ; break;
- case 'Q': temp.value =VALUE_QUEEN  ; break;
- case 'K': temp.value =VALUE_KING   ; break;
- case 'A': temp.value =VALUE_ACE    ; break;
- default : temp.value = 0;
-}
-}
+ case 'A': temp.value =14           ; break;
+ case 'Q': temp.value =12           ; break;
+ case 'J': temp.value =11           ; break;
+ default : temp.value = value_let - '0';break;
 
-   else {
-     printf("invalid value \n");
-     exit (EXIT_FAILURE);
-   }
-
+ }
    switch (suit_let) {
    case 's' : temp.suit = SPADES ; break;
    case 'h' : temp.suit = HEARTS ; break;
    case 'd' : temp.suit = DIAMONDS ; break;
    case 'c' : temp.suit = CLUBS ; break;
-   default : exit(EXIT_FAILURE);
-   }
+   default  : temp.suit = NUM_SUITS;break:;} 
+   assert_card_valid(temp);
    return temp;
 }
 
 
 card_t card_from_num(unsigned c) {
   card_t temp;
-  unsigned suitNum = c /13;
-  unsigned valNum = c % 13;
-  switch (suitNum) {
+  unsigned value = c % 13 + 2;
+  temp.value = value;
+  unsigned suit = c /13;
+  
+  switch (suit) {
   case 0: temp.suit = SPADES; break;
   case 1: temp.suit = HEARTS; break;
   case 2: temp.suit = DIAMONDS; break;
   case 3: temp.suit = CLUBS ; break;
-  default : exit(EXIT_FAILURE);
+  default :temp.suit = NUM_SUITS; break;
       }
-  temp.value = valNum + 2;
     return temp;
 }
