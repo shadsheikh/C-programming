310262f6ebfd8916a0f5b3ebe5ce7e82886cfae3
Coursera Learner <nobody@nowhere.nul>
2020-06-19 09:51:20 +0000

    Did assignment 27

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -29,9 +29,9 @@ int checkinput(FILE * f){
 
 void rotate(char matrix[10][10]){
   for (int i = 0;i < 10 ; i++) {
-    for (int j = i;j < 10;j++){
+    for (int j = i ; j < 10;j++){
       char temp =matrix[i][j];
-      matrix[i][j]=matrix[j][i]
+      matrix[i][j]=matrix[j][i];
 	matrix[j][i]=temp;
     }
   }
